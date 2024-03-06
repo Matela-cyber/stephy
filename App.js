2564f98aaca59ec1db98d1a54109791dcf7c8230
Matela-cyber <143181642+Matela-cyber@users.noreply.github.com>
2024-03-06 18:32:37 +0200

    update1

--- App.js
+++ App.js
@@ -1,39 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image} from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-<View style={styles.top}><View style={styles.leftcon}>      <Image source={require('./profile.jpg')}
-      style={styles.profilepicture}/>
-      <View style={styles.text}>
-        <Text>Reitumetse Mporoane
-      </Text>
-    <View><Text>10hrs</Text></View>
+      <View style={styles.top}>
+        <View style={styles.leftcon}>
+          <Image
+            source={require("./profile.jpg")}
+            style={styles.profilepicture}
+          />
+          <View style={styles.text}>
+            <Text>Reitumetse Mporoane</Text>
+            <Text>10hrs</Text>
+          </View>
+        </View>
+        <Text>...</Text>
       </View>
-      </View><Text>...</Text></View>
 
-      
-      
-      
-      
-      
-      <Text>“Having A Best Friend” is a story of two best friends who got into a fight,
-         but later the first friend saves his friend’s life.
-          The origin of the story cannot be determined as this story has been passed on through generations.</Text>
-      <Image source={require('./body.jpg')} style={styles.bodypicture}/>
+      <Text>
+        “Having A Best Friend” is a story of two best friends who got into a
+        fight, but later the first friend saves his friend’s life. The origin of
+        the story cannot be determined as this story has been passed on through
+        generations.
+      </Text>
+      <Image source={require("./body.jpg")} style={styles.bodypicture} />
       <Text>62 likes</Text>
-      <Text></Text>
-      <View style={styles.bottom}><View><AntDesign name="like1" size={24} color="black" /><Text>Likes</Text>
+      <View style={styles.bottom}>
+        <View>
+          <AntDesign name="like1" size={24} color="black" />
+          <Text>Likes</Text>
+        </View>
+        <View>
+          <FontAwesome name="comment-o" size={24} color="black" />
+          <Text>Comment</Text>
+        </View>
+        <View>
+          <MaterialCommunityIcons
+            name="share-outline"
+            size={24}
+            color="black"
+          />
+          <Text>Share</Text>
+        </View>
       </View>
-      <View><FontAwesome name="comment-o" size={24} color="black" /><Text>Comment</Text>
-      </View>
-      <View><MaterialCommunityIcons name="share-outline" size={24} color="black" /><Text>Share</Text>
-      </View></View>
-      <StatusBar style="auto" />
+      <StatusBar style="auto;" />
     </View>
   );
 }
@@ -41,33 +55,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 50,
+  },
+  profilepicture: {
     padding: 10,
-   
+    width: 40,
+    height: 40,
+    borderRadius: 50,
   },
-  profilepicture:{
-    padding:10,
-    width:40,
-    height:40,
-    borderRadius:50,
+  bodypicture: {
+    padding: 10,
+    height: 500,
+    width: 500,
   },
-  bodypicture:{
-    padding:10,
-    height:500,
-    width:500,
-
-  },
-  top:
-  {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-
+  top: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   leftcon: {
-    flexDirection:'row',
+    flexDirection: "row",
   },
   bottom: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
